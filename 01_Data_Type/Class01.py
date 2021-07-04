bc006b40f5c92e03f2ac871320f4de5403bc4b1f
Lancelot.exe <67932290+EdgarPlasencia@users.noreply.github.com>
2021-07-04 18:48:21 -0500

    Create Class01.py

---
+++ 01_Data_Type/Class01.py
@@ -0,0 +1,48 @@
+#Author: Edgar Plasencia
+
+#Data type/Tipos de datos
+
+#A data type is an attribute of data which tells the compiler or interpreter how the programmer intends to use the data.
+#Un tipo de dato es un atributo de datos que le dice al compilador o intérprete cómo el programador pretende usar los datos.
+
+#Integer: These are the common numbers.
+#Floating-point number: These are used to reprecent a decimal number. Here we have the FLOAT and DOUBLE datay type.
+#Character: A single unit of information, based on ASCII code.
+#String: Sequense of characters, the most comon example or use are words and text.
+#Boolean: Data whit two posible values, true or false. 
+
+#Entero: Estos son los números comunes.
+#Número de coma flotante: se utilizan para volver a repetir un número decimal. Aquí tenemos el tipo de datos FLOAT y DOUBLE.
+#Caracter: una sola unidad de información, basada en el código ASCII.
+#String: Secuencia de caracteres, el ejemplo o uso más común son las palabras y el texto.
+#Boolean: Datos con dos valores posibles, verdadero o falso.
+
+#Variables declaration
+#Declaracion  de variables
+
+#Integer
+num01=15
+#Float or double
+num02=2.1548
+#Character
+num03='A'
+#String
+num04="Hello"
+#Boolean
+num05=True
+
+#Data print
+#Impresion de datos
+print(num01)
+print(num02)
+print(num03)
+print(num04)
+print(num05)
+
+#Using a python function to determine data type
+#Usando una funcion de python para determinar el tipo de dato
+print(type(num01))
+print(type(num02))
+print(type(num03))
+print(type(num04))
+print(type(num05))
